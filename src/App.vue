e77d933c8289e3d6e6cb688b177773a786e1d149
Felipe Scanda <felipescanda@gmail.com>
2025-06-10 15:29:06 -0400

    Actualizacion: Importar SignIn.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import ChangePassword from './components/ChangePassword.vue';
-
+import SignIn from './components/SignIn.vue';
 </script>
 
 <template>
   <div>
+    <SignIn />
     <ChangePassword />
   </div>
 </template>
