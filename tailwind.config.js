f4a4e41711c63cc27d1548b0cba5dc8a4e0375e5
MartinJaque <jaqueolivosmartin@gmail.com>
2025-06-10 17:20:26 -0400

    stable version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,37 @@
 /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+//   theme: {
+//     extend: {
+//       colors: {
+//         maincolor: '[#2148C0]',
+//       },
+//     },
+//   },
+//   plugins: [],
+// }
+
+// module.exports = {
+//   purge: ['./index.html', './src/*/.{vue,js,ts,jsx,tsx}'],
+//   darkMode: false, // or 'media' or 'class'
+//   theme: {
+//     extend: {},
+//   },
+//   variants: {
+//     extend: {},
+//   },
+//   plugins: [],
+//
+// }
+
 export default {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
-        maincolor: '[#2148C0]',
+        maincolor: '#2148C0', // corregido
       },
     },
-  },
-  plugins: [],
-}
-
-module.exports = {
-  purge: ['./index.html', './src/*/.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
 }
